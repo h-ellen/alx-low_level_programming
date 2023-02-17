02b8f52f6ad47699b7f01a457ee9dfd1333f761b
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-17 12:00:50 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - shows how the program exited
+ * return: ends the execution of a function
+ *
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	char charType;
