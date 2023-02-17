3c7741b5b061881150863f333be01ea37bf43c4b
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-17 11:44:50 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - shows how the program exited
+ * return: ends the execution of a function
+ *
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
