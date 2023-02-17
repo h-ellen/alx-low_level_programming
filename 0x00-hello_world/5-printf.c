c9e2712a8ac093ab54646f1b0a93d3de4418ec6f
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-17 11:56:15 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - shows how the program exited
+ * return: ends the execution of a function
+ *
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
