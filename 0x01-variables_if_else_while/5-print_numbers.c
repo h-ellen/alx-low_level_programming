16d3950f3a72f91cfcedc77d49a6e0eebf2d6858
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-17 17:34:24 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - shows how the program exited
+ * return: ends the execution of a function
+ *
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
