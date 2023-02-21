6c1f3a0f3b5587e79151439c0dace1e6237eea5d
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-21 10:21:52 -0800

    trial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - shows how the program exited
+ * return: ends the execution of a function
+ *
+ * Return: print all single digit base 10 numbers
+ */
+
+int main(void)
+{
+	char ch = '_putchar'
+		putchar(ch);
+	putchar ('\n');
+	return (0);
+}
