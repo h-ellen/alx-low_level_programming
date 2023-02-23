c1ac311415581e70524b494dc9356a94803706d9
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-23 13:17:20 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,20 +5,22 @@
  * @n: is the integer for the paramaters of my function
  * Return: 0
  */
+
 void print_line(int n)
 {
- if (n <= 0)
- {
- _putchar('\n');
- } else
- {
- int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
 
- for (i = 1; i <= n; i++)
- {
- _putchar('_');
- }
- _putchar('\n');
- }
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 
 }
