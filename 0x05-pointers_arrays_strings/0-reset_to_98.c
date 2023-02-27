c399e2e6734ccbb334e4fa8d53e664a6728bc8a0
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-02-27 14:53:34 -0800

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** return_to_98 - function takes a pointer to an int as parameter
+/*
+ * return_to_98 - function takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * return: 0
  */
