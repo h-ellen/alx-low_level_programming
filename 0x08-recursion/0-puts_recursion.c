d8038b5a47d541f5f8196e1ab53f1529246f1663
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-03-13 03:46:40 -0700

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
-		_putchar(*s++);
+		_putchar(*s);
+		_puts_recursion(s+1);
 	}
 
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
