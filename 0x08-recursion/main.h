f9010b1c264ee5284a31787b44a8097b7c900370
Hellen Arinaitwe <harinaitwe336@gmail.com>
2023-03-13 04:02:51 -0700

    main file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,11 @@
 #define main_h
 void _puts_recursion(char *s);
 int _putchar(char c);
+void _print_rev_recursion(char *s);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
 
 #endif
